e56af4efaa33f6f13dfa356ae7d23f2c9b50da13
taxmad360 <taxmad360@gmail.com>
2026-03-07 17:27:59 +0100

    Update sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,14 +1,14 @@
-const CACHE_NAME = 'taxmad-v1';
+const CACHE_NAME = 'taxmad-cache-v1';
 const urlsToCache = ['/'];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request).then((response) => response || fetch(event.request))
   );
 });
