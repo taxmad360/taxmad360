2ba2065641ee31e04a7c74a9d556ff642612740c
taxmad360 <taxmad360@gmail.com>
2026-03-10 13:35:04 +0100

    Update test-db.js

--- test-db.js
+++ test-db.js
@@ -7,6 +7,7 @@ async function probarConexion() {
   if (error) console.error("Error al insertar:", error);
   else console.log("¡Cliente insertado con éxito!", data);
 }
+import { supabase } from '../lib/utils/supabase'; // Asegura que la ruta sea correcta
 
-// ¡Añade esto debajo!
+console.log("¿Qué es supabase?", supabase); // Si esto imprime 'undefined' o 'null', el problema está en la exportación// ¡Añade esto debajo!
 probarConexion();
